6a81f34609b283b9eb65b0a92799f8bf4f3a949c
=杨洲 <1594396225@qq.com>
2023-01-09 12:46:00 +0800

    new file:   .gitignore 	new file:   "vue2/1.vue\345\237\272\347\241\200/14.\351\235\236\345\215\225\346\226\207\344\273\266\347\273\204\344\273\266/1.\345\237\272\346\234\254\344\275\277\347\224\250.html" 	new file:   "vue2/1.vue\345\237\272\347\241\200/14.\351\235\236\345\215\225\346\226\207\344\273\266\347\273\204\344\273\266/2.\345\207\240\344\270\252\346\263\250\346\204\217\347\202\271.html" 	new file:   "vue2/1.vue\345\237\272\347\241\200/14.\351\235\236\345\215\225\346\226\207\344\273\266\347\273\204\344\273\266/3.\347\273\204\344\273\266\347\232\204\345\265\214\345\245\227.html" 	new file:   "vue2/1.vue\345\237\272\347\241\200/14.\351\235\236\345\215\225\346\226\207\344\273\266\347\273\204\344\273\266/4.Vuecomponent\346\236\204\351\200\240\345\207\275\346\225\260.html" 	new file:   "vue2/1.vue\345\237\272\347\241\200/14.\351\235\236\345\215\225\346\226\207\344\273\266\347\273\204\344\273\266/5.\344\270\200\344\270\252\351\207\215\350\246\201\347\232\204\345\206\205\347\275\256\345\205\263\347\263\273.html" 	new file:   "vue2/1.vue\345\237\272\347\241\200/15.\345\215\225\346\226\207\344\273\266\347\273\204\344\273\266/App.vue" 	new file:   "vue2/1.vue\345\237\272\347\241\200/15.\345\215\225\346\226\207\344\273\266\347\273\204\344\273\266/School.vue" 	new file:   "vue2/1.vue\345\237\272\347\241\200/15.\345\215\225\346\226\207\344\273\266\347\273\204\344\273\266/Student.vue" 	new file:   "vue2/1.vue\345\237\272\347\241\200/15.\345\215\225\346\226\207\344\273\266\347\273\204\344\273\266/index.html" 	new file:   "vue2/1.vue\345\237\272\347\241\200/15.\345\215\225\346\226\207\344\273\266\347\273\204\344\273\266/main.js"

---
+++ vue2/1.vue基础/15.单文件组件/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <School></School>
+    </div>
+</template>
+
+<script>
+// 引入组件
+import School from './School'
+import Student from './Student'
+
+export default {
+    name:'App',
+    template:`<App></App>`,
+    components:{
+        School,
+        Student,
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue2/1.vue基础/15.单文件组件/School.vue
@@ -0,0 +1,41 @@
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2>学校名称：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+        <button @click="showX">showX</button>
+      </div>
+</template>
+
+
+<script>
+// 组件交互相关的代码(数据、方法等)，引入后要暴露才能使用
+/* 必须要暴露(ES6)
+1.分别暴露 export const School = Vue.extend() (暴露多个的时候推荐) 引入：import {???/} from ???
+2.统一暴露 export{School}         (暴露多个的时候推荐) 引入：import {???/} from ???
+3.默认暴露 export default School (暴露一个的时候推荐) 引入：import ??? from ???
+*/
+export default {
+      name:'School', //和文件名一样
+      data() {
+        return {
+          name: '尚硅谷',
+          address: '成都',
+        }
+      },
+      methods: {
+        showX() {
+          console.log(this.x) //99
+        },
+      },
+    }
+     
+</script>
+
+
+<style>
+/* 组件的样式 */
+.demo{
+    background-color: pink;
+}
+</style>
